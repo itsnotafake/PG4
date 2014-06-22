d10109c66500386094ab1b0d317d81512f205b20
Simon Gibbs <s.gibbs@samsung.com>
2014-06-21 17:23:52 -0700

    Adding French support

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,9 @@ public class HelloWorld {
 	
 	public static void main(String[] Args){
 		String s = "Hello Cool World";
+		String s1 = "Bonjour Tout Le Monde";
 		System.out.println(s);
+		System.out.println(s1);
 	}
 
 }
