97e8c867a1bf67ede1501b716ca3ccb1b0fcf85c
U-Devin-Laptop\Devin <Devin@Devin-Laptop.(none)>
2014-06-21 20:35:09 -0400

    Minor changes o languages

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,9 +3,11 @@ public class HelloWorld {
 	
 	public static void main(String[] Args){
 		String s = "Hello Cool World";
-		String s1 = "Bonjour Tout Le Monde";
+		String s1 = "Bonjour Tout Le Monde Cool";
+		String s2 = "Haagan Daaz";
 		System.out.println(s);
 		System.out.println(s1);
+		System.out.println(s2);
 	}
 
 }
